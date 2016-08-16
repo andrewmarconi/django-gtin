e3e9c56bad1768faae2b2e6d2099ca263d60263c
Andrew Marconi <andrew@559labs.com>
2016-08-16 16:47:37 +0300

    Initial commit. File structure and metadata only. No functionality yet.

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name = 'django-gtin',
+    packages = ['django-gtin'],
+    version = '0.1',
+
+    include_package_data=True,
+    license='GPLv3',
+    description='Adds a GTIN (Global Trade Item Number) field and provides additional EAN, UPC, etc. functionality.',
+    long_description=README,
+    author = 'Andrew Marconi',
+    author_email = 'andrew@marconimedia.com',
+    url = 'https://github.com/andrewmarconi/django-gtin',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+#        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.4',
+        'Framework :: Django :: 1.5',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
